5fb8888b08ec89e34d3e9be58310b4cd39d21ec0
Tianshu Yu <tianshu.yu@duke.edu>
2023-11-29 17:03:48 -0500

    Eliminate a redudant function

--- rainfall_simulation.h
+++ rainfall_simulation.h
@@ -2,10 +2,6 @@
 #define RAINFALL_SIMULATION_H_
 #include <cstddef>
 
-// simulate rainfall at one step and return if all water is absorbed
-int simulateOneStep(float *waterAboveGround, float *waterAbsorbed, const int *elevation_map, 
-const size_t rain_time, const float absorption_rate, const size_t dim_landscape, const size_t time);
-
 // return the number of steps taken for all the water to be absorbed
 int simulateRainFall(float *waterAboveGround, float *waterAbsorbed, const int *elevation_map,
 const size_t rain_time, const float absorption_rate, const size_t dim_landscape);
